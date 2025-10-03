a977ceb05b739e059042a320e53fdf7e5bcf553a
hookbait515-source <hookbait515@gmail.com>
2025-10-03 10:20:10 -0500

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -1,17 +1,1 @@
-index.html
-style.css
-<html>
-<head>
-<title>Webpage</title>
-<style>
-body {
-	width: 800px;
-	font-family:Helvetica;
-	font-size:28px
-	color:black;
-	background-color:white;
-}
-</style>
-</head>
-<body>
-<p>
+
